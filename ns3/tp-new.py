de3512c59e8fec2e5f472b3fd25368bdd4e81535
Prajval <26prajval98@gmail.com>
2019-02-03 19:10:13 +0530

    add --proto=tcp/udp

--- ns3/tp-new.py
+++ ns3/tp-new.py
@@ -7,6 +7,7 @@ parser.add_argument('inFile', type=str)
 parser.add_argument('outFileBytes', type=str)
 parser.add_argument('outFilePackets', type=str)
 parser.add_argument('--startTime', type=float, default=0)
+parser.add_argument('--proto', type=str, default='tcp')
 parser.add_argument('--packetSize', type=float)
 parser.add_argument('--destPort', type=int)
 parser.add_argument('--destIp', type=str)
@@ -21,6 +22,7 @@ packetSize     = args.packetSize
 destIp         = args.destIp
 flowLabel      = args.flowLabel
 destPort       = args.destPort
+proto          = args.proto
 
 if not os.path.exists(os.path.dirname(outFileBytes)):
     try:
@@ -37,7 +39,7 @@ if not os.path.exists(os.path.dirname(outFilePackets)):
         if exc.errno != errno.EEXIST:
             raise
 
-os.system ("tshark -r " + file_name +" -T fields -e frame.time_relative -e frame.len -e ip.dst -e tcp.dstport -e udp.dstport> " + file_name[0:file_name.rindex('.')] + ".csv")
+os.system ("tshark -r " + file_name +" -T fields -e frame.time_relative -e frame.len -e ip.dst -e " + proto + ".dstport > " + file_name[0:file_name.rindex('.')] + ".csv")
 fBytes         = open (outFileBytes, 'w')
 fPackets       = open (outFilePackets, 'w')
 p              = startTime
@@ -48,14 +50,11 @@ valPacket      = 0
 totalval       = 0
 totalvalPacket = 0
 
-print(packetSize, " is the packet size")
-
 with open(file_name[0:file_name.rindex('.')] + '.csv') as fq:
     fp = fq.readlines()
     for line in fp:
         # print(line.split('\t')[3].strip (), line.split('\t')[2].strip (), float(line.split('\t')[0]))
         if line.split('\t')[3].strip () != '' :
-            print(int (line.split('\t')[3].strip ()), str(line.split('\t')[2]).strip ())
             if (destIp == None or str(line.split('\t')[2]).strip () == destIp) and (float(line.split('\t')[1]) == packetSize or packetSize == None) and float(line.split('\t')[0]) >= startTime and (destPort == None or destPort == int (line.split('\t')[3].strip ())):
                 # print(line.split('\t')[3].strip ())
                 new_time = float(line.split('\t')[0])
@@ -72,8 +71,8 @@ with open(file_name[0:file_name.rindex('.')] + '.csv') as fq:
                     old_time = new_time
                     val = 0
                     valPacket = 0
-        # print(new_time)
+            # print(new_time)
     print flowLabel + ' In Mbps: ' + str(totalval * 8.0/(1000.0 * 1000.0)/(new_time - startTime)) + ' In Packets: ' + str(totalvalPacket/(new_time - startTime))
 fBytes.close()
 fPackets.close()
-# os.remove(file_name[0:file_name.rindex('.')] + '.csv')
+os.remove(file_name[0:file_name.rindex('.')] + '.csv')
