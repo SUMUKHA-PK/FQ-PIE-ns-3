02e188d9f8812a316fafd0b24b5bc6ff022c5461
Tejas111 <tejaskumark11@gmail.com>
2019-04-26 17:26:02 +0530

    merging

--- ns3/aqm-traffic.sh
+++ ns3/aqm-traffic.sh
@@ -1,18 +1,17 @@
- #!/bin/bash
+#!/bin/bash
 
-mkdir ~/FqPieTCP10
+mkdir ~/FqPieTCP5
 counter=0
 for var in 1 3 5 7 10 12 15 18 21 28 35 43 50 70 120 200 800 1200 3000 5000 7000 13000 17000 25000 35000
     do
     counter=$((counter+1))
-(       NS_GLOBAL_VALUE="RngRun=$var" ./waf --run fq-pie-10-up  	
-) | tee -a ~/FQPIE/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP10/q.plotme
-    mkdir ~/FqPieTCP10/$counter
-    mkdir ~/FqPieTCP10/$counter/pcap
-    mv ~/FQPIE/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP10/pcap ~/FqPieTCP10/$counter
-    mv ~/FQPIE/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP10/q.plotme ~/FqPieTCP10/$counter
+(       NS_GLOBAL_VALUE="RngRun=$var" ./waf --run fq-pie-5-up  	
+) | tee -a ~/FQPIE/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP5/q.plotme
+    mkdir ~/FqPieTCP5/$counter
+    mkdir ~/FqPieTCP5/$counter/pcap
+    mv ~/FQPIE/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP5/pcap ~/FqPieTCP5/$counter
+    mv ~/FQPIE/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP5/q.plotme ~/FqPieTCP5/$counter
 done
-
 
 # counter=0
 # for var in 1 3
@@ -38,3 +37,4 @@ done
 #     mv /home/some_guyy/pk/git/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP50/pcap /home/some_guyy/pk/FqPieTCP50/$counter
 #     mv /home/some_guyy/pk/git/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP50/q.plotme /home/some_guyy/pk/FqPieTCP50/$counter
 # done
+
