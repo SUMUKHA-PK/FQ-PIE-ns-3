8bb392cb5571db4791564a52504717c6f88af1bf
SUMUKHA-PK <sumukhapk46@gmail.com>
2019-04-26 10:29:24 +0530

    Script changed

--- ns3/aqm-traffic.sh
+++ ns3/aqm-traffic.sh
@@ -1,15 +1,16 @@
  #!/bin/bash
 
+mkdir ~/FqPieTCP10
 counter=0
 for var in 1 3 5 7 10 12 15 18 21 28 35 43 50 70 120 200 800 1200 3000 5000 7000 13000 17000 25000 35000
     do
     counter=$((counter+1))
-(       NS_GLOBAL_VALUE="RngRun=$var" ./waf --run fq-pie-5-up  	
-) | tee -a /home/some_guyy/pk/git/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP5/q.plotme
-    mkdir /home/some_guyy/pk/FqPieTCP5/$counter
-    mkdir /home/some_guyy/pk/FqPieTCP5/$counter/pcap
-    mv /home/some_guyy/pk/git/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP5/pcap /home/some_guyy/pk/FqPieTCP5/$counter
-    mv /home/some_guyy/pk/git/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP5/q.plotme /home/some_guyy/pk/FqPieTCP5/$counter
+(       NS_GLOBAL_VALUE="RngRun=$var" ./waf --run fq-pie-10-up  	
+) | tee -a ~/FQPIE/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP10/q.plotme
+    mkdir ~/FqPieTCP10/$counter
+    mkdir ~/FqPieTCP10/$counter/pcap
+    mv ~/FQPIE/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP10/pcap ~/FqPieTCP10/$counter
+    mv ~/FQPIE/FQ-PIE-ns-3/ns3/Evaluation/FqPieTCP10/q.plotme ~/FqPieTCP10/$counter
 done
 
 
